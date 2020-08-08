02eea2c8f9f3155d3e040a0bd5fab53e0c99ac12
clouding <ghost0436@gmail.com>
2020-08-08 14:38:59 +0800

    feat: implement index handler

--- main.go
+++ main.go
@@ -3,19 +3,43 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/cloudingcity/dcard/pkg/ratelimit"
 )
 
-const defaultPort = "8080"
+const (
+	defaultPort        = "8080"
+	defaultRateLimit   = 60
+	defaultRateTimeout = 60
+)
+
+var limiter *ratelimit.Limiter
 
 func main() {
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = defaultPort
 	}
+	limit, _ := strconv.Atoi(os.Getenv("RATE_LIMIT"))
+	if limit == 0 {
+		limit = defaultRateLimit
+	}
+	timeout, _ := strconv.Atoi(os.Getenv("RATE_TIMEOUT"))
+	if timeout == 0 {
+		timeout = defaultRateTimeout
+	}
 
-	http.HandleFunc("/", hello)
+	limiter = ratelimit.New(ratelimit.Config{
+		Max:     limit,
+		Timeout: timeout,
+	})
+
+	http.HandleFunc("/", index)
 
 	log.Printf("Started listening on %s\n", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
@@ -23,6 +47,31 @@ func main() {
 	}
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "hello")
+func index(w http.ResponseWriter, req *http.Request) {
+	key := realIP(req)
+	hit, err := limiter.Hit(key)
+	if err != nil {
+		w.Header().Set("Retry-After", strconv.Itoa(hit.ResetTime))
+		w.WriteHeader(http.StatusTooManyRequests)
+		fmt.Fprintln(w, "Error")
+		return
+	}
+
+	w.Header().Set("X-RateLimit-Limit", strconv.Itoa(limiter.Max))
+	w.Header().Set("X-RateLimit-Remaining", strconv.Itoa(hit.Remaining))
+	w.Header().Set("X-RateLimit-Reset", strconv.Itoa(hit.ResetTime))
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "requests: "+strconv.Itoa(hit.Count))
+}
+
+func realIP(req *http.Request) string {
+	xForwardedFor := req.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(req.RemoteAddr)); err == nil {
+		return ip
+	}
+	return "127.0.0.1"
 }
---
+++ main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/cloudingcity/dcard/pkg/ratelimit"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndex(t *testing.T) {
+	limiter = ratelimit.New(ratelimit.Config{
+		Max:     3,
+		Timeout: 1,
+	})
+
+	req := httptest.NewRequest("GET", "http://example.com", nil)
+	w := httptest.NewRecorder()
+
+	index(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "3", resp.Header.Get("X-RateLimit-Limit"))
+	assert.Equal(t, "2", resp.Header.Get("X-RateLimit-Remaining"))
+	assert.Equal(t, "1", resp.Header.Get("X-RateLimit-Reset"))
+	assert.Equal(t, "requests: 1\n", string(body))
+
+	index(w, req)
+	index(w, req)
+
+	w = httptest.NewRecorder()
+	index(w, req)
+	resp = w.Result()
+	body, _ = ioutil.ReadAll(resp.Body)
+	assert.Equal(t, http.StatusTooManyRequests, resp.StatusCode)
+	assert.Equal(t, "1", resp.Header.Get("Retry-After"))
+	assert.Equal(t, "Error\n", string(body))
+}
+
+func TestRealIP(t *testing.T) {
+	t.Run("X-Forwarded-For exists", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "http://example.com", nil)
+		req.Header.Set("X-Forwarded-For", "9.9.9.9, 0.0.0.0, 0.0.0.0")
+		want := "9.9.9.9"
+		got := realIP(req)
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("Remote Addr", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "http://example.com", nil)
+		req.RemoteAddr = "10.10.10.10:8080"
+		want := "10.10.10.10"
+		got := realIP(req)
+		assert.Equal(t, want, got)
+	})
+}
